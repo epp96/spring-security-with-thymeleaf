78ff2fd81cb550e33b9150160fe9d9476c0ff8ca
epp96 <epp96@vt.edu>
2020-05-29 19:36:59 -0400

    initial commit

---
+++ src/main/java/com/example/springsecuritywiththymeleaf/SpringSecurityWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritywiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecuritywiththymeleaf/SpringSecurityWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritywiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
